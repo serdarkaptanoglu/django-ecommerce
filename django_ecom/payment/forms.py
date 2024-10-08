7caf63f36cf45b0b9ff2860330f3c3e052f17c86
serdarkaptanoglu <serdarkaptanog57@gmail.com>
2024-10-09 02:55:13 +0300

    added order,order item and checkout.html

--- django_ecom/payment/forms.py
+++ django_ecom/payment/forms.py
@@ -4,7 +4,7 @@ from .models import ShippingAddress
 
 class ShippingForm(forms.ModelForm):
     shipping_full_name = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Full Name'}), required=True)
-    shipping_email = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Full Name'}), required=True)
+    shipping_email = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email Address'}), required=True)
     shipping_address1 = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Address1'}), required=True)
     shipping_address2 = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Address2'}), required=False)
     shipping_country = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Address2'}), required=True)
--- django_ecom/payment/models.py
+++ django_ecom/payment/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from store.models import Product
 
 
 class ShippingAddress(models.Model):
@@ -10,7 +11,7 @@ class ShippingAddress(models.Model):
     shipping_address2 = models.CharField(max_length=250, null=True, blank=True)
     shipping_country = models.CharField(max_length=250)
     shipping_city = models.CharField(max_length=250)
-    shipping_district = models.CharField(max_length=200)
+    shipping_district = models.CharField(max_length=200, null=True, blank=True)
     shipping_postcode = models.CharField(max_length=250, null=True, blank=True)
 
     class Meta:
@@ -18,3 +19,26 @@ class ShippingAddress(models.Model):
 
     def __str__(self):
         return f'{self.user} - Shipping Address - {str(self.id)}'
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    full_name = models.CharField(max_length=250)
+    email = models.EmailField(max_length=250)
+    shipping_address = models.TextField(max_length=15000)
+    amount_paid = models.DecimalField(max_digits=10, decimal_places=2)
+    date_order = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Order - {str(self.id)}'
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, null=True, blank=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    quantity = models.PositiveIntegerField(default=1)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+
+    def __str__(self):
+        return f'Order Item - {str(self.id)}'

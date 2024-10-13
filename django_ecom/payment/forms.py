e63cc4cb9a61fda205804de384a242a2c582383b
serdarkaptanoglu <serdarkaptanog57@gmail.com>
2024-10-13 16:41:13 +0300

    Attach Order Items

--- django_ecom/payment/forms.py
+++ django_ecom/payment/forms.py
@@ -7,7 +7,7 @@ class ShippingForm(forms.ModelForm):
     shipping_email = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email Address'}), required=True)
     shipping_address1 = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Address1'}), required=True)
     shipping_address2 = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Address2'}), required=False)
-    shipping_country = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Address2'}), required=True)
+    shipping_country = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Country'}), required=True)
     shipping_city = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'City'}), required=True)
     shipping_district = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'District'}), required=True)
     shipping_postcode = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Postcode'}), required=False)
@@ -16,3 +16,12 @@ class ShippingForm(forms.ModelForm):
         model = ShippingAddress
         fields = ['shipping_full_name', 'shipping_email', 'shipping_address1', 'shipping_address2', 'shipping_country', 'shipping_city', 'shipping_district', 'shipping_postcode']
         exclude = ['user',]
+
+
+class PaymentForm(forms.Form):
+    card_name = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Name on Card'}), required=True)
+    card_number = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Card Number'}), required=True)
+    exp_date = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Expiration Date'}), required=True)
+    cvv = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'CVV'}), required=True)
+    postcode = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Postcode'}), required=False)
+
--- django_ecom/payment/views.py
+++ django_ecom/payment/views.py
@@ -1,7 +1,79 @@
 from django.shortcuts import render, redirect
 from cart.cart import Cart
-from payment.forms import ShippingForm
-from payment.models import ShippingAddress
+from payment.forms import ShippingForm, PaymentForm
+from payment.models import ShippingAddress, Order, OrderItem
+from django.contrib import messages
+from store.models import Product
+
+
+def process_order(request):
+    if request.POST:
+        cart = Cart(request)
+        cart_products = cart.get_prods
+        quantities = cart.get_quants
+        totals = cart.cart_total()
+        payment_form = PaymentForm(request.POST or None)
+        my_shipping = request.session.get('my_shipping')
+        full_name = my_shipping['shipping_full_name']
+        email = my_shipping['shipping_email']
+        amount_paid = totals
+        shipping_address = f"{my_shipping['shipping_address1']}\n{my_shipping['shipping_address2']}\n{my_shipping['shipping_country']}\n{my_shipping['shipping_city']}\n{my_shipping['shipping_district']}\n{my_shipping['shipping_postcode']}"
+        # Create an Order
+        if request.user.is_authenticated:
+            # Logged in
+            user = request.user
+            # Create Order
+            create_order = Order(user=user, full_name=full_name, email=email, shipping_address=shipping_address, amount_paid=amount_paid)
+            create_order.save()
+            # Get the order ID
+            order_id = create_order.pk
+            # Get product Info
+            for product in cart_products():
+                product_id = product.id
+                if product.is_sale:
+                    price = product.sale_price
+                else:
+                    price = product.price
+                for key, value in quantities().items():
+                    if int(key) == product.id:
+                        create_order_item = OrderItem(order_id=order_id, product_id=product_id, user=user, quantity=value, price=price)
+                        create_order_item.save()
+
+            messages.success(request, 'Odeme Yapildi...')
+            return redirect('home')
+
+        else:
+            create_order = Order(full_name=full_name, email=email, shipping_address=shipping_address, amount_paid=amount_paid)
+            create_order.save()
+            messages.success(request, 'Odeme Yapildi...')
+            return redirect('home')
+
+    else:
+        messages.info(request, 'Erisim Reddildi !!!')
+        return redirect('home')
+
+
+def billing_info(request):
+    if request.POST:
+        cart = Cart(request)
+        cart_products = cart.get_prods
+        quantities = cart.get_quants
+        totals = cart.cart_total()
+        my_shipping = request.POST
+        request.session['my_shipping'] = my_shipping
+        if request.user.is_authenticated:
+            billing_form = PaymentForm()
+            return render(request, 'payment/billing_info.html', {'cart_products': cart_products, 'quantities': quantities, 'totals': totals, 'shipping_form': request.POST, 'billing_form': billing_form})
+        else:
+            billing_form = PaymentForm()
+
+            return render(request, 'payment/billing_info.html', {'cart_products': cart_products, 'quantities': quantities, 'totals': totals, 'shipping_form': request.POST, 'billing_form': billing_form})
+
+        shipping_form = request.POST
+        return render(request, 'payment/billing_info.html', {'cart_products': cart_products, 'quantities': quantities, 'totals': totals, 'shipping_form': shipping_form})
+    else:
+        messages.info(request, 'Erisim Reddildi !!!')
+        return redirect('home')
 
 
 def checkout(request):
